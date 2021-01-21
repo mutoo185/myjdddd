41d72ca4120c99126181fd1dde41176641202805
mutoo185 <76713957+mutoo185@users.noreply.github.com>
2021-01-21 14:45:19 +0800

    Update loon.js

--- loon.js
+++ loon.js
@@ -125,6 +125,7 @@ cron "30 8 * * *" script-path=https://raw.githubusercontent.com/LXK9301/jd_scrip
 cron "55 23 * * *" script-path=https://raw.githubusercontent.com/LXK9301/jd_scripts/master/jd_unsubscribe.js,tag=取关京东店铺商品
 #注销京东会员卡
 cron "55 23 * * *" script-path=https://raw.githubusercontent.com/LXK9301/jd_scripts/master/jd_unbind.js,tag=注销京东会员卡
-
+#京东炸年兽AR
+cron "0 8 * * *" script-path=https://raw.githubusercontent.com/LXK9301/jd_scripts/master/jd_nian_ar.js,tag=京东炸年兽AR
 
 
