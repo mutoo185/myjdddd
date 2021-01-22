b2f25d60ccfbff64c801c42f6e1e4328b0145f3f
mutoo185 <76713957+mutoo185@users.noreply.github.com>
2021-01-22 08:45:28 +0800

    Update loon.js

--- loon.js
+++ loon.js
@@ -8,7 +8,6 @@
 hostname = api.m.jd.com, draw.jdfcloud.com, jdjoy.jd.com, account.huami.com
 #京东多合一签到
 cron "30 0 * * *" tag=京东多合一签到, script-path=https://raw.githubusercontent.com/NobyDa/Script/master/JD-DailyBonus/JD_DailyBonus.js
-http-request https:\/\/api\.m\.jd\.com\/client\.action.*functionId=signBean tag=获取京东Cookie, script-path=https://raw.githubusercontent.com/NobyDa/Script/master/JD-DailyBonus/JD_DailyBonus.js
 
 
 # 京豆变动通知
